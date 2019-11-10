d79a44d7c5a64c3c168e458244566199c2429c62
sungman han <hansung926@gmail.com>
2019-11-10 15:08:57 +0900

    router crud singlepage sample

--- 04.node_router/app.js
+++ 04.node_router/app.js
@@ -1,9 +1,16 @@
+/* npm moduel */
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-var methodOverride = require('method-override');
-
 const app = express();
+const path = require("path");
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const morgan = require('morgan');
+
+/* node mondule */
+const bodyParser = require("body-parser");
+const methodOverride = require('method-override');
+
+/* default setting */
 const port = 3000;
 const log = console.log;
 
@@ -20,17 +27,26 @@ app.set("view engine","pug");
 app.set("views",path.join(__dirname,"views"));
 app.locals.pretty = true;
 
+/* Morgan setting */
+let logDirectory = path.join(__dirname, 'log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+let accessLogStream = rfs('access.log', {
+    interval: '1d', // rotate daily
+    path: logDirectory
+});
+app.use(morgan('combined', { stream: accessLogStream }))
+
 /* Method orverride setting */ 
 app.use(methodOverride('X-HTTP-Method')) //          Microsoft
 app.use(methodOverride('X-HTTP-Method-Override')) // Google/GData
 app.use(methodOverride('X-Method-Override')) //      IBM
 app.use(methodOverride((req, res) => {
-if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    // look in urlencoded POST bodies and delete it
-    var method = req.body._method
-    delete req.body._method
-    return method
-}
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
 }));
 
 /* Router setting */
--- 04.node_router/router/rest.js
+++ 04.node_router/router/rest.js
@@ -1,20 +1,44 @@
 const express = require("express");
+const path = require("path")
 const router = express.Router();
+const {sqlExec, ...db} = require(path.join(__dirname,"../modules/mysql-conn"));
+const {zp,isoDate,jsToiso} = require("../modules/util-doc")
+const {alertLoc} = require("../modules/util-loc")
+
+let sql = "";
+let result = "";
+
 
 router.get("/",(req,res) => {
-    res.send("<h1>REST-READ</h1>")
+    (async () => {
+        sql = "SELECT * FROM rest ORDER BY id DESC";
+        result = await sqlExec(sql);
+        res.render("rest/crud.pug",{datas:jsToiso(result[0],"wdate")});
+    })();
 });
 
 router.post("/",(req,res) => {
-    res.send("<h1>REST-POST</h1>")
+    (async () => {
+        let username = req.body.username;
+        let wdate = isoDate(new Date(),1);
+        sql = "INSERT INTO rest SET username=?, wdate=?";
+        let vals = [username,wdate];
+        result = await sqlExec(sql,vals);
+        if (result[0].affectedRows > 0){
+            //res.json(result)
+            res.send(alertLoc("생성 성공.", "rest/"));
+        }else{
+
+        }
+    })();
 });
 
 router.put("/",(req,res) => {
-    res.send("<h1>ADMIN-UPDATE</h1>")
+    res.send("<h1>ADMIN-UPDATE</h1>");
 });
 
 router.delete("/",(req,res) => {
-    res.send("<h1>ADMIN-DELETE</h1>")
+    res.send("<h1>ADMIN-DELETE</h1>");
 });
 
 module.exports = router
