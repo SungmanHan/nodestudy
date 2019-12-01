ad7b2d380b148b7b055e8f943ac925c4ae2439b4
sungman han <hansung926@gmail.com>
2019-12-01 14:55:55 +0900

    mysql2 and sequelize added code

--- 08.multer/app.js
+++ 08.multer/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var multerRouter = require('./routes/multer');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get("/", (req,res) => {
   res.send("ROOT");
---
+++ 09.session/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+router.get("/join", (req,res,next) => {
+  res.render("join")
+})
+
+module.exports = router;
